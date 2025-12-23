6ba87cdfe5ec1d8143df8f43fbe4d84a64be3bbc
Agyemang8172 <godfredagyemang31052005@gmail.com>
2025-12-23 08:01:34 +0000

    New updates

--- weather app/src/App.jsx
+++ weather app/src/App.jsx
@@ -1,47 +1,54 @@
-import { useState,useEffect } from 'react'
-import WeatherInput from './Component/WeatherInput';
+  import { useState,useEffect } from 'react'
+  import WeatherInput from './Component/WeatherInput';
+  import WeatherInformation from './Component/WeatherInformation';
 
 
-function App() {
-  const [city, setCity] = useState('')
-  const [Loading,setLoading] =  useState(false);
-  const [weather,setWeather] = useState(null);
-  const [error,setError]  = useState(null)
-  
-  useEffect(()=>  {
-    const fetchWeather  =  async ()  => {
-      try {
-          setLoading(true)
-           if (!city) return
-          const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-
-
-
-      const response =  await fetch(`https://api.openweathermap.org/data/2.5/weather?q=CITY_NAME&appid=${API_KEY}`)
-        if (!response.ok)  throw new Error('fetch failed');
-        
-          const data = await response.json();
-            setWeather(data);
-             } catch (err) {
-            setError(err.message)
-      }  finally {
-        setLoading(false)
-
-      }
-    }
-    fetchWeather()
-  },[city])
+  function App() {
+    const [city, setCity] = useState('')
+    const [loading,setLoading] =  useState(false);
+    const [weather,setWeather] = useState(null);
+    const [error,setError]  = useState(null)
+    
+    useEffect(()=>  {
+      const fetchWeather  =  async ()  => {
+        try { 
+            if (!city) return
+            setLoading(true)
+            const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+          const response =  await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+          if (!response.ok)  throw new Error('fetch failed');
+          
+            const data = await response.json();
+            
+              setWeather(data);
+              } catch (err) {
+              setError(err.message)
+        }  finally {
+          setLoading(false)
+          }
+            }
+      fetchWeather()
+   
+    },[city])
  
 
-  return(
+  
 
-  <div>
-    <WeatherInput 
-     
-    />
-    <WeatherInformation />
-  </div>
-  )
-}
+    return(
 
-export default App
+    <div className='flex flex-col  justify-center items-center  min-h-screen bg-gray-100   '> 
+      
+            <WeatherInput 
+              onSearch = {setCity}
+            />
+            <WeatherInformation 
+              onResult  = {weather}
+              onLoading = {loading}
+              onError= {error}
+            />
+          
+    </div>
+    )
+  } 
+
+  export default App
--- weather app/src/Component/WeatherInput.jsx
+++ weather app/src/Component/WeatherInput.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 
-const WeatherInput = () => {
-    const [city,setCity] = useState(null)
+const WeatherInput = ({onSearch}) => {
+    const [inputCity,setInputCity] = useState('')
+
 
     const handleSubmit = (e)  => {
         e.preventDefault()
-        if ( city.trim() !== "") {
-            fetchWeather(city)
+        if ( inputCity.trim() !== "") {
+             onSearch(inputCity)
         }
     }
 
@@ -16,12 +17,12 @@ const WeatherInput = () => {
       <input 
         type='text' 
         placeholder='Enter city name'
-        value={city}
-        onChange={(e)=>setCity(e.target.value)}
-
-        
-      />
-      <button type='submit'>
+        value={inputCity}
+        onChange={(e)=>setInputCity(e.target.value)}
+        className='w-72 px-4 py-2 rounded-md border border-gray-400 outline-none focus:border-blue-300  shadow-sm'
+    />
+      <button type='submit'
+      className=' ml-5 px-4 py-2 bg-blue-500  text-white  rounded-md hover:bg-blue-600 transition'>
           Search
       </button>
    </form>
